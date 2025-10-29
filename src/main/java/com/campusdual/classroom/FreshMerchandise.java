82f22d684ee7226a6af1ca9917c2ad709d30013f
Inés <ines.ramos@campusdual.com>
2025-10-29 10:43:18 +0100

    solved exercise 15

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -4,12 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
-    private final Date expirationDate;
+    private Date expirationDate;
 
-    public FreshMerchandise(String name, String uniqueId, String responsibleId, Date expirationDate) {
-        super(name, uniqueId, responsibleId);
-        this.expirationDate = expirationDate;
-    }
 
     public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
         super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
@@ -17,17 +13,30 @@ public class FreshMerchandise extends Merchandise {
     }
 
     public Date getExpirationDate() {
-        return expirationDate;
+        return this.expirationDate;
     }
+
     public String getFormattedDate(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         return sdf.format(date);
     }
-    public Object getSpecificData() {
-        return "ExpirationDate: " + getFormattedDate(this.expirationDate);
-    }
+
+    public String getSpecificData(){
+        String formattedDate = getFormattedDate(this.expirationDate);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nombre del producto: ").append(this.name).append("\n");
+        sb.append("ID unico: ").append(this.uniqueId).append("\n");
+        sb.append("Responsable: ").append(this.responsibleId).append("\n");
+        sb.append("Ubicacion: ").append(getLocation()).append("\n");
+        sb.append("Cantidad: ").append(this.quantity).append("\n");
+        sb.append("Fecha de caducidad: ").append(formattedDate);
+
+        return sb.toString();
+}
+
     public void printSpecificData(){
         System.out.println(getSpecificData());
-
     }
 }
+
